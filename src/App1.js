3cf10d9db5b9a8f4b001c2c755cfbf0886e77a65
victorjoseph02 <dakivictorjoseph@gmail.com>
2025-03-25 23:08:49 +0530

    Delete

--- src/App1.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function App1(props) {
-  return <div style={{ backgroundColor: "yellow" }}>
-    {props.name}-{props.age}
-    </div>;
-}
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,16 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import "./Register.css"
+import { Link } from "react-router-dom";
 export default function Register() {
+  const [users, setUsers] = useState([]);
+  const [user, setUser] = useState({});
+  const handleSubmit = () => {
+    setUsers([...users, user]);
+  };
+  const handleDelete = (value) => {
+    setUsers(users.filter((element) => element !== value));
+  };
   return (
-    <div>
-      <h2>Registration Form</h2>
-      <p><input type="text" placeholder='Enter name'></input></p>
-      <p><input type="text" placeholder='Email address'></input></p>
-      <p><input type="password" placeholder='New password'></input></p>
-      <p><button>Submit</button></p>
-      <p>
-        <Link to="../login">Login</Link>
-      </p>
+    <div className="App-Register-Row">
+      <div>
+        <h2>Registration Form</h2>
+        <p>
+          <input
+            type="text"
+            placeholder="Enter name"
+            onChange={(e) => setUser({ ...user, name: e.target.value })}
+          ></input>
+        </p>
+        <p>
+          <input
+            type="text"
+            placeholder="Email address"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          ></input>
+        </p>
+        <p>
+          <input
+            type="password"
+            placeholder="New password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          ></input>
+        </p>
+        <p>
+          <button onClick={handleSubmit}>Submit</button>
+        </p>
+        <p>
+          <Link to="../login">Already a member? Log In</Link>
+        </p>
+      </div>
+      <div>
+        {
+          users && users.map((value,index)=>(
+            <li>{value.name}-{value.email}-{value.password} <button onClick={() => handleDelete(value)}>Delete</button></li>
+          ))
+        }
+      </div>
     </div>
-  ) 
+  );
 }
